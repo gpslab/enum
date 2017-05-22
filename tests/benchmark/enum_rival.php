fd91f3639d05cc1583eecca95cfacfe19d99b7ce
Peter Gribanov <wcode404@gmail.com>
2017-05-22 19:56:01 +0300

    add benchmark to readme

--- tests/benchmark/enum_rival.php
+++ tests/benchmark/enum_rival.php
@@ -25,7 +25,7 @@ $N = $input->getFirstArgument();
 
 // Reflection
 $sw->start('ref', 'Reflection enum');
-for ($i = 0; $i < $N; ++$i) {
+for ($iteration = 0; $iteration < $N; ++$iteration) {
     $a = AbcRef::a();
     $a->isA();
 
@@ -42,7 +42,7 @@ echo $sw->stop('ref').PHP_EOL;
 
 // Reflection (no magic)
 $sw->start('nmag', 'Reflection enum no magic');
-for ($i = 0; $i < $N; ++$i) {
+for ($iteration = 0; $iteration < $N; ++$iteration) {
     $d = DefRef::byValue(DefRef::D);
     $d->equals(DefRef::byValue(DefRef::D));
 
@@ -59,7 +59,7 @@ echo $sw->stop('nmag').PHP_EOL;
 
 // myclabs/php-enum
 $sw->start('mc', 'MyClabs enum');
-for ($i = 0; $i < $N; ++$i) {
+for ($iteration = 0; $iteration < $N; ++$iteration) {
     $a = AbcMyClabs::A();
     $a->equals(AbcMyClabs::A());
 
@@ -76,7 +76,7 @@ echo $sw->stop('mc').PHP_EOL;
 
 // marc-mabe/php-enum
 $sw->start('mm', 'MarcMabe enum');
-for ($i = 0; $i < $N; ++$i) {
+for ($iteration = 0; $iteration < $N; ++$iteration) {
     $a = AbcMarcMabe::A();
     $a->is(AbcMarcMabe::A());
 
@@ -93,7 +93,7 @@ echo $sw->stop('mm').PHP_EOL;
 
 // marc-mabe/php-enum (no magic)
 $sw->start('mmnm', 'MarcMabe enum no magic');
-for ($i = 0; $i < $N; ++$i) {
+for ($iteration = 0; $iteration < $N; ++$iteration) {
     $a = AbcMarcMabe::byValue(AbcMarcMabe::A);
     $a->is(AbcMarcMabe::byValue(AbcMarcMabe::A));
 
@@ -110,7 +110,7 @@ echo $sw->stop('mmnm').PHP_EOL;
 
 // happy-types/enumerable-type
 $sw->start('ht', 'HappyTypes enum');
-for ($i = 0; $i < $N; ++$i) {
+for ($iteration = 0; $iteration < $N; ++$iteration) {
     $a = AbcHappyTypes::A();
     $a->equals(AbcHappyTypes::A());
 
@@ -127,7 +127,7 @@ echo $sw->stop('ht').PHP_EOL;
 
 // Explicit
 $sw->start('exp', 'Explicit enum');
-for ($i = 0; $i < $N; ++$i) {
+for ($iteration = 0; $iteration < $N; ++$iteration) {
     $a = AbcExp::byValue(AbcExp::A);
     $a->equals(AbcExp::byValue(AbcExp::A));
 
