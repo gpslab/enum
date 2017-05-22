d965e0456ba43fb5b6b92b0f066284681aaa0878
Peter Gribanov <wcode404@gmail.com>
2017-05-22 12:56:19 +0300

    test AbcExp

---
+++ tests/AbcExpTest.php
@@ -0,0 +1,147 @@
+<?php
+
+/**
+ * Carousel project.
+ *
+ * @author Peter Gribanov <PGribanov@1tv.com>
+ */
+
+namespace GpsLab\Component\Enum\Tests;
+
+
+use GpsLab\Component\Enum\Tests\Enum\AbcExp;
+
+class AbcExpTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var array
+     */
+    private $choices = [
+        AbcExp::A => 'acme.demo.abc.a',
+        AbcExp::B => 'acme.demo.abc.b',
+        AbcExp::C => 'acme.demo.abc.c',
+    ];
+
+    public function testChoices()
+    {
+        $this->assertEquals($this->choices, AbcExp::choices());
+    }
+
+    public function testValues()
+    {
+        $values = [];
+        foreach (AbcExp::values() as $value) {
+            $values[] = $value->value();
+        }
+        sort($values);
+
+        $this->assertEquals(array_keys($this->choices), $values);
+    }
+
+    /**
+     * @return array
+     */
+    public function getChoicesData()
+    {
+        $data = [];
+        foreach ($this->choices as $value => $title) {
+            $data[] = [$value, $title];
+        }
+
+        return $data;
+    }
+
+    /**
+     * @dataProvider getChoicesData
+     *
+     * @param string $value
+     * @param string $title
+     */
+    public function testCreate($value, $title)
+    {
+        $this->assertTrue(AbcExp::isValid($value));
+
+        $channel = AbcExp::byValue($value);
+
+        $this->assertEquals($value, $channel->value());
+        $this->assertEquals($title, (string) $channel);
+        $this->assertTrue($channel->equals(AbcExp::byValue($value)));
+    }
+
+    /**
+     * @return array
+     */
+    public function getSerializeData()
+    {
+        $data = [];
+        foreach ($this->choices as $value => $title) {
+            $ser = serialize($value);
+            $data[] = [
+                $value,
+                sprintf('C:%d:"%s":%d:{%s}', strlen(AbcExp::class), AbcExp::class, strlen($ser), $ser),
+            ];
+        }
+
+        return $data;
+    }
+
+    /**
+     * @dataProvider getSerializeData
+     *
+     * @param string $value
+     * @param string $result
+     */
+    public function testSerialize($value, $result)
+    {
+        $this->assertEquals($result, serialize(AbcExp::byValue($value)));
+    }
+
+    /**
+     * @dataProvider getSerializeData
+     *
+     * @param string $value
+     * @param string $result
+     */
+    public function testUnserialize($value, $result)
+    {
+        $this->assertEquals(AbcExp::byValue($value), unserialize($result));
+    }
+
+    /**
+     * @expectedException \LogicException
+     */
+    public function testClone()
+    {
+        $channel = AbcExp::byValue(AbcExp::A);
+        $channel = clone $channel;
+    }
+
+    /**
+     * @expectedException \GpsLab\Component\Enum\Exception\OutOfEnumException
+     */
+    public function testValueNotSupported()
+    {
+        AbcExp::byValue('foo');
+    }
+
+    public function testIsA()
+    {
+        $this->assertEquals(AbcExp::A, AbcExp::byValue(AbcExp::A)->value());
+        $this->assertTrue(AbcExp::byValue(AbcExp::A)->equals(AbcExp::byValue(AbcExp::A)));
+        $this->assertFalse(AbcExp::byValue(AbcExp::A)->equals(AbcExp::byValue(AbcExp::B)));
+    }
+
+    public function testIsB()
+    {
+        $this->assertEquals(AbcExp::B, AbcExp::byValue(AbcExp::B)->value());
+        $this->assertTrue(AbcExp::byValue(AbcExp::B)->equals(AbcExp::byValue(AbcExp::B)));
+        $this->assertFalse(AbcExp::byValue(AbcExp::B)->equals(AbcExp::byValue(AbcExp::A)));
+    }
+
+    public function testIsC()
+    {
+        $this->assertEquals(AbcExp::C, AbcExp::byValue(AbcExp::C)->value());
+        $this->assertTrue(AbcExp::byValue(AbcExp::C)->equals(AbcExp::byValue(AbcExp::C)));
+        $this->assertFalse(AbcExp::byValue(AbcExp::C)->equals(AbcExp::byValue(AbcExp::A)));
+    }
+}
