ad70d34610f0892db2a95343bc26ff36ca19d84e
Peter Gribanov <wcode404@gmail.com>
2019-10-23 10:49:30 +0300

    optimize detect constants for PHP < 7.1

--- src/ConstantDetector.php
+++ src/ConstantDetector.php
@@ -17,12 +17,12 @@ class ConstantDetector
      */
     public static function detect($class)
     {
-        $constants = [];
         $reflection = new \ReflectionClass($class);
 
         if (PHP_VERSION_ID >= 70100) {
             // Since PHP-7.1 visibility modifiers are allowed for class constants
             // for enumerations we are only interested in public once.
+            $constants = [];
             foreach ($reflection->getReflectionConstants() as $constant) {
                 if ($constant->isPublic()) {
                     $constants[$constant->getName()] = $constant->getValue();
@@ -33,10 +33,6 @@ class ConstantDetector
         }
 
         // In PHP < 7.1 all class constants were public by definition
-        foreach ($reflection->getConstants() as $constant_name => $constant_value) {
-            $constants[$constant_name] = $constant_value;
-        }
-
-        return $constants;
+        return $reflection->getConstants();
     }
 }
